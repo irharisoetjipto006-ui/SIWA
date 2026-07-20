8dfa6a3ab8ea37c15e71cbfec8078ab495f07ba9
Hari Soetjipto <ir.hari.soetjipto.006@gmail.com>
2026-07-21 06:35:21 +0700

    Sprint 1: Berhasil membuat fitur siwa import whatsapp

--- tools/update.sh
+++ tools/update.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-if [ -z "$1" ]; then
-    echo ""
-    echo "Cara pakai:"
-    echo "./tools/update.sh \"Pesan commit\""
-    exit 1
-fi
-
-echo ""
-echo "======================================="
-echo "         UPDATE KE GITHUB"
-echo "======================================="
-echo ""
-
+SIWA_HOME="$HOME/SIWA"
+cd "$SIWA_HOME"
 git add .
-
-git commit -m "$1"
-
-git push origin main
-
-echo ""
-echo "======================================="
-echo "           SELESAI"
-echo "======================================="
+git commit -m "${1:-Update SIWA}"
+git push
